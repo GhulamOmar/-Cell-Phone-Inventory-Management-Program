f74098c009901af24097b172bb72d86ae00813a0
Omar Ahmad <gomar@dmacc.edu>
2023-09-28 14:14:39 -0500

    Add files via upload
    Cell Phone Inventory Management Program
    
    Description:
    The Cell Phone Inventory Management Program is a C# application designed to assist in managing a company's inventory of cell phones. Developed by Omar Ahmad, this program operates on Windows 10 using Microsoft Visual Studio 2017 and demonstrates key concepts taught in the CIS 169 C# course.
    
    Key Features:
    
    User-friendly interface: The program provides an intuitive interface for users to input cell phone information.
    Object-oriented design: It employs object-oriented programming principles to create and manage cell phone objects.
    Data validation: The application includes data validation to ensure accurate input, particularly for the phone's price.
    Object creation: Upon clicking the "Create Object" button, the program creates a CellPhone object, assigns the entered data to its properties (brand, model, and price), and displays this information on the interface.
    Detailed information: It displays the brand, model, and price of the created cell phone object in corresponding labels.
    Exit functionality: Users can easily close the program using the "Exit" button.

---
+++ Cell Phone Test/Cell Phone Test/CellPhone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cell_Phone_Test
+{
+    class CellPhone
+    {
+        // fields
+        private string _brand; // this cell phone brand.
+        private string _model; // this cell phone model.
+        private decimal _price; // cell phone retail price.
+        public CellPhone() // constructor.
+        {
+        _brand="";
+        _model="";
+       _price=0m;
+            }
+    public string Brand // Brand property.
+    {
+        get { return _brand; }
+        set { _brand = value; }
+    }
+    public string Model// Model property.
+    {
+        get { return _model; }
+        set { _model = value; }
+    }
+    public decimal Price // Price property.
+    {
+        get { return _price; }
+        set { _price = value; }
+
+    }
+    }
+    }
+
---
+++ Cell Phone Test/Cell Phone Test/Form1.cs
@@ -0,0 +1,68 @@
+﻿/** =========================================================
+ Name: Omar Ahmad
+ Operating System (Windows 10)
+ Microsoft Visual Studio 2017
+ CIS 169 C# 
+M09: CellPhone Tutorial 9-2.
+This program manages the company's inventory of cell phones. It allows the user to enter cell phone data into text boxes.
+And then click the create object button. it craetes an object of the cell phone class and assign the data from text boxes
+to the object's properties then displays the values of the object's properties in model, prices, brand's labels.
+ I attest that this is my original work.
+ I have not used unauthorized source code, either modified or unmodified.
+ I have not given other fellow student(s) access to my program.
+=========================================================== **/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cell_Phone_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The GetPhoneData method accepts the  acellphone object as an argument.
+        // It assigns the data entered by the user to the object's properities.
+        private void GetPhoneData(CellPhone phone)
+        {
+            decimal price;// temporary veraible to hold the price.
+            phone.Brand = brandTextBox.Text;// this gets the phon's brand.
+            phone.Model = modelTextBox.Text;
+            if (decimal.TryParse(priceTextBox.Text, out price))// this gets the phone's price
+            {
+                phone.Price = price;
+            }
+            else
+            {
+                MessageBox.Show("Invalid price");// shows an error message.
+            }
+        }
+        private void createObjectButton_Click(object sender, EventArgs e)// click event handler.
+        {
+            CellPhone myphone = new CellPhone();// Create a cellphone object.
+            GetPhoneData(myphone);// this gets the phone data.
+            // Displays the phone data.
+            brandLabel.Text = myphone.Brand;
+            modelLabel.Text = myphone.Model;
+            priceLabel.Text = myphone.Price.ToString("c");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)// click event handler.
+        {
+            this.Close();// this close the form.
+        }
+    }
+}
+
+        
+    
+        
+    
+
